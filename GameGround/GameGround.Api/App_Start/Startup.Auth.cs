6c03fdabe22166e5acce46804f41f4395bc72bee
myamolane <myamolane@outlook.com>
2017-05-22 00:24:35 +0800

    building

---
+++ GameGround/GameGround.Api/App_Start/Startup.Auth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+
+namespace GameGround.Api
+{
+    public partial class Startup
+    {
+        public static OAuthBearerAuthenticationOptions OAuthOptions { get; private set; }
+
+        static Startup()
+        {
+            OAuthOptions = new OAuthBearerAuthenticationOptions();
+        }
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            
+        }
+    }
+}
